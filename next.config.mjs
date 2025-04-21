db1d2feb6fea624219bc99dc8da98e9fb605267b
NikolayViktorovich <n.golubtsov05@bk.ru>
2025-04-21 19:46:09 +0500

    upd next
    upd next

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	env: {
-	  SERVER_URL: process.env.SERVER_URL,
-	},
-	reactStrictMode: true,
-	swcMinify: true,
-	images: {
-	  domains: [],
-	},
-	output: 'standalone',
-  };
-  
-  export default nextConfig;
+		SERVER_URL: process.env.SERVER_URL
+	}
+	// webpack: config => {
+	// 	config.plugins.push(
+	// 		new RsdoctorWebpackPlugin({
+	// 			// plugin options
+	// 		})
+	// 	)
+
+	// 	return config
+	// }
+}
+
+// const nextConfigModify = withNextBundleAnalyzer(nextConfig)
+
+export default nextConfig
