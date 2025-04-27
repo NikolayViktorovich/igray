f3638ca800c16d36287d17e4e5a6259bb020474c
NikolayViktorovich <n.golubtsov05@bk.ru>
2025-04-27 21:17:25 +0500

    next config
    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
 	env: {
-		SERVER_URL: process.env.SERVER_URL
-	}
-	// webpack: config => {
-	// 	config.plugins.push(
-	// 		new RsdoctorWebpackPlugin({
-	// 			// plugin options
-	// 		})
-	// 	)
-
-	// 	return config
-	// }
-}
-
-// const nextConfigModify = withNextBundleAnalyzer(nextConfig)
-
-export default nextConfig
+	  SERVER_URL: process.env.SERVER_URL,
+	},
+	experimental: {
+	  serverActions: true,
+	  trustedHosts: [
+		'igray24.ru',
+	  ],
+	},
+  };
+  
+  export default nextConfig;
