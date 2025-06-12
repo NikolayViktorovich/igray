ee45252b25433fb3574c8ddec70b9a3a47313d7d
NIKOLAY VIKTOROVICH <145424511+NikolayViktorovich@users.noreply.github.com>
2025-06-12 19:02:31 +0500

    upd 2
    upd 2

--- src/modules/steam-replenishment/ui/ComissionPanel.tsx
+++ src/modules/steam-replenishment/ui/ComissionPanel.tsx
@@ -1,61 +1,11 @@
 import { FC } from 'react'
 
-import { symbols } from '@/shared/constants/symbols'
-
 import type { Currencies } from '../model/types'
 
 interface IProps {
-	currency: Currencies
+  currency: Currencies
 }
 
-export const CommissionPanel: FC<IProps> = ({ currency }) => {
-	const commissions = {
-		RUB: [
-			{
-				commission: 22,
-				price: 100
-			},
-			{
-				commission: 20,
-				price: 1000
-			},
-			{
-				commission: 18,
-				price: 3000
-			}
-		],
-		KZT: [
-			{
-				commission: 22,
-				price: 500
-			},
-			{
-				commission: 20,
-				price: 5000
-			},
-			{
-				commission: 18,
-				price: 15000
-			}
-		],
-		USD: [
-			{
-				commission: 22,
-				price: 5
-			},
-			{
-				commission: 20,
-				price: 10
-			},
-			{
-				commission: 18,
-				price: 30
-			}
-		]
-	}
-
-  return (
-    <>
-    </>
-  )
+export const CommissionPanel: FC<IProps> = () => {
+  return <div>Панель без комиссии</div>
 }
